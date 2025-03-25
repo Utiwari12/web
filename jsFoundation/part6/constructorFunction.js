717b2d9439ae50c757352728cbda3254c53372d3
Utiwari12 <motioniitjeeinstitute@gmail.com>
2025-03-25 22:47:12 +0530

    web development

---
+++ jsFoundation/part6/constructorFunction.js
@@ -0,0 +1,55 @@
+
+// function greet(name){
+//     console.log("Hello, ", name )
+//     //return name
+
+// }
+// greet("Upendra")
+
+
+// function Persion(name, age, city){
+//     this.name = name
+//     this.age = age
+//     this.city = city
+// }
+
+// function Car(make, model, age){
+//     this.make = make
+//     this.model = model
+//     this.age = age
+// }
+
+//How to create object using constructor function
+// let myCar = new Car("Toyota", "Corolla", 2024 )
+// console.log(myCar);
+
+// let myCar1 = new Car("Tata", "Safari", 2024 )
+// console.log(myCar1);
+
+// let myPersion = new Persion("Upendra", 45, "Patna")
+// console.log(myPersion);
+
+function Tea(type, price){
+    this.type = type
+    this.price = price
+    this.describe = function(){
+        return `This is a cup of tea of type ${this.type} and price is ${this.price}`
+    }
+
+}
+
+let myTea = new Tea("Lemon Tea", 20)
+console.log(myTea.describe());
+
+
+function Animal(species){
+    this.species = species
+}
+
+Animal.prototype.sound = function(){
+    return `${this.species} makes a sound`
+}
+
+let dog = new Animal("Dog")
+console.log(dog.sound());
+
