f940a6e52aa74edc8a4d5cfc523eb3f971d87410
Utiwari12 <motioniitjeeinstitute@gmail.com>
2025-03-28 22:32:18 +0530

    part7

---
+++ jsFoundation/part7/script.js
@@ -0,0 +1,9 @@
+console.log("hello from script");
+//console.log(window);
+//console.log(document);
+console.log(document.body);
+console.log(document.body.childNodes);
+console.log(document.body.childNodes[0]);
+//console.log(document.body.childNodes[0].nodeName);
+//console.log(document.body.childNodes[1].nodeName);
+//console.log(document.body.childNodes[2].nodeName);
