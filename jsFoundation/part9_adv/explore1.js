ec2fac170a02835846d3bb91fa046de2e9563cc2
Utiwari12 <motioniitjeeinstitute@gmail.com>
2025-04-04 00:20:15 +0530

    Asynchronous function

---
+++ jsFoundation/part9_adv/explore1.js
@@ -0,0 +1,15 @@
+console.log("Upendra");
+
+function sayHello(){
+    console.log("I would like to say Hello");
+}
+
+setTimeout(() => {
+    sayHello();
+}, 4000);
+
+console.log("Tiwari");
+for (let index = 0; index < 10; index++) {
+    console.log(index);
+    
+}
